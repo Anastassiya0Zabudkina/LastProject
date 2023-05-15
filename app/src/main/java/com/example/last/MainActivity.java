a67472b749b5891da97b0cd3bd941150520c6447
Anastassiya Zabudkina <124874555+Anastassiya0Zabudkina@users.noreply.github.com>
2023-05-15 23:11:37 +0600

    az/first commit

--- app/src/main/java/com/example/last/MainActivity.java
+++ app/src/main/java/com/example/last/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.last;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
@@ -27,11 +35,37 @@ public class MainActivity extends AppCompatActivity {
                     // Обработка нажатия на элемент "Работы выпускников"
                     return true;
                 case R.id.action_more:
-                    // Обработка нажатия на элемент "Еще"
+                    openDrawer();
                     return true;
                 default:
                     return false;
             }
         });
+    }
+
+    private void openDrawer() {
+        drawerLayout.openDrawer(GravityCompat.END);
+    }
+
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.END);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            closeDrawer();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
